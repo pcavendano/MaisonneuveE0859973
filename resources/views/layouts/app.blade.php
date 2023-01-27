7985ccb3cac2af35698ddb4584348faaa5dd9b5c
Pedro Contreras Avendano <pedro.contreras.avendano@gmail.com>
2023-01-27 14:30:07 -0500

    Mise a jour du ficheir app pour ajouter le titre, uen nouvelle police ainsi que le footercaca

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,11 +1,19 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <title>Title</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>{{ config('app.name') }}</title>
+    <!-- Fonts -->
+    <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=sw" rel="stylesheet" >
+    <style>
+        body
+        {
+            font-family: 'Nunito';
+        }
+    </style>
 </head>
 <body>
-{{-- View Output --}}
-@yield('content ')
+@yield('content')
 </body>
 </html>
