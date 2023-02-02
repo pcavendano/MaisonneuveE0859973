ae045e5f5a5eaf159ead23d33537dd941f2834f3
Pedro Contreras Avendano <pedro.contreras.avendano@gmail.com>
2023-02-02 16:05:42 -0500

    Set up de la création d'un theme custom avec BootstrapÃ avec le fichier ressources/sass/app.scss

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -9,7 +9,7 @@
 
     <!-- Le css Bootstrap -->
     <link rel="stylesheet" href="{{ asset('css/app.css') }}">
-    
+
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/boxicons/2.1.0/css/boxicons.min.css" integrity="sha512-pVCM5+SN2+qwj36KonHToF2p1oIvoU3bsqxphdOIWMYmgr4ZqD3t5DjKvvetKhXGc/ZG5REYTT6ltKfExEei/Q==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/MaterialDesign-Webfont/5.3.45/css/materialdesignicons.css" integrity="sha256-NAxhqDvtY0l4xn+YVa6WjAcmd94NNfttjNsDmNatFVc=" crossorigin="anonymous" />
 
@@ -22,6 +22,7 @@
 </head>
 <body>
 @yield('content')
+<!-- Le javascript Bootstrap -->
 <script src="{{asset('js/app.js')}}"></script>
 <script src="{{asset('js/bootstrap.js')}}"></script>
 </body>
