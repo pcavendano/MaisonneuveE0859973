ae899a5b1a6a8da648da7cee3bad019f2bda18a5
Pedro Contreras Avendano <pedro.contreras.avendano@gmail.com>
2023-02-06 16:09:17 -0500

    Mise a jour du fichier webpack.mix.js pour ajouter le fichier app.js dans le fichier public

---
+++ resources/js/app.js
@@ -0,0 +1,6 @@
+let file = document.getElementById('file');
+file.addEventListener('change', (event) => {
+    let image = document.getElementById('output');
+    console.log("aquis");
+    image.src = URL.createObjectURL(event.target.files[0]);
+})
--- webpack.mix.js
+++ webpack.mix.js
@@ -12,5 +12,6 @@ const mix = require('laravel-mix');
  */
 
 mix.js('resources/js/bootstrap.js', 'public/js');
+mix.js('resources/js/app.js', 'public/js/app.js');
 
 mix.sass('resources/sass/app.scss', 'public/css').sourceMaps();
