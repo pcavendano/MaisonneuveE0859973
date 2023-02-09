a46e35ed70e11fd3dde463353839f4152e0de02f
Pedro Contreras Avendano <pedro.contreras.avendano@gmail.com>
2023-02-08 20:19:11 -0500

    ÃInstallation de browser.sync pour refrechir la page automatiquement avec la commande npm run watch

--- webpack.mix.js
+++ webpack.mix.js
@@ -13,5 +13,5 @@ const mix = require('laravel-mix');
 
 mix.js('resources/js/bootstrap.js', 'public/js');
 mix.js('resources/js/app.js', 'public/js/app.js');
-
+mix.browserSync('http://127.0.0.1/cadriciel_21647/tp1_laravel/MaisonneuveE0859973/public/');
 mix.sass('resources/sass/app.scss', 'public/css').sourceMaps();
